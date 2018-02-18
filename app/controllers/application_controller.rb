b0f5ef8d9d359cb1648056a424f9087c7affc071
Rauof M <abdelrauof197@gmail.com>
2018-02-18 17:10:03 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
   end
   
   post '/teams' do 
-    team = Team.new(params[:team])
+    @team = Team.new(params[:team])
     params[:team][:members].each { |attributes| 
       Members.new(attributes)
     }
