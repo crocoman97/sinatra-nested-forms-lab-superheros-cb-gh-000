33f3170df69f4286182b7ca4e83766e060ebf327
Rauof M <abdelrauof197@gmail.com>
2018-02-18 16:57:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,23 @@
+
 require 'sinatra/base'
 
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
-
+  set :views, Proc.new { File.join(root, "../views/") }
+  
+  get '/' do 
+    erb :index
+  end
+  
+  post '/teams' do 
+    team = Team.new(params[:team])
+    params[:team][:members].each { |attributes| 
+      Members.new(attributes)
+    }
+    @members = Members.all
+    
+    erb :team
+    
+  end
 
 end
